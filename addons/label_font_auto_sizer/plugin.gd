1d2f0ca841e82374c7e2f6b86ba57d746101eaf1
LuisEscorza <109640033+LuisEscorza@users.noreply.github.com>
2024-04-07 21:15:36 -0400

    Fix issue not finding all the nodes at startup
    Had to re-add the is_connected() checks since they were actually necessary.

--- addons/label_font_auto_sizer/plugin.gd
+++ addons/label_font_auto_sizer/plugin.gd
@@ -13,15 +13,18 @@ func _ready() -> void:
 
 ## Called whenever a new node is added to the scene. Calls for a label check.
 func _on_child_entered_tree(node: Node) -> void:
-	node.child_entered_tree.connect(_on_child_entered_tree.bind()) ##Keeps checking for nested children, looking for labels.
-	node.child_exiting_tree.connect(_on_child_exiting_tree.bind())
+	if !node.child_entered_tree.is_connected(_on_child_entered_tree.bind()): ##Keeps checking for nested children, looking for labels.
+			node.child_entered_tree.connect(_on_child_entered_tree.bind())
+	if !node.child_exiting_tree.is_connected(_on_child_exiting_tree.bind()):
+			node.child_exiting_tree.connect(_on_child_exiting_tree.bind())
 	_check_label(node)
 
 
 ## Called when changing scene. Gets the new root and calls for a node scan in the tree.
 func _on_scene_changed(root: Node) -> void:
 	if _current_root != null and is_instance_valid(_current_root):
-		_current_root.child_entered_tree.disconnect(_on_child_entered_tree.bind())
+		if _current_root.child_entered_tree.is_connected(_on_child_entered_tree.bind()):
+			_current_root.child_entered_tree.disconnect(_on_child_entered_tree.bind())
 	_current_root = root
 	await _set_root(root)
 	call_deferred("_check_nodes_in_scene")
@@ -29,14 +32,18 @@ func _on_scene_changed(root: Node) -> void:
 
 ## Gets called whenever any node is deleted, and when user changes scene.
 func _on_child_exiting_tree(node: Node) -> void:
-	node.child_exiting_tree.disconnect(_on_child_exiting_tree)
-	node.child_entered_tree.disconnect(_on_child_entered_tree)
+	if node.child_exiting_tree.is_connected(_on_child_exiting_tree):
+		node.child_exiting_tree.disconnect(_on_child_exiting_tree)
+	if node.child_entered_tree.is_connected(_on_child_entered_tree):
+		node.child_entered_tree.disconnect(_on_child_entered_tree)
 
 
 ## Gets called whenever a label is deleted, and whenever user changes scene.
 func _on_label_exiting_tree(label: Node) -> void:
-	label.tree_exiting.disconnect(_on_label_exiting_tree.bind(label))
-	label.script_changed.disconnect(_on_label_script_changed.bind(label))
+	if label.tree_exiting.is_connected(_on_label_exiting_tree.bind(label)):
+		label.tree_exiting.disconnect(_on_label_exiting_tree.bind(label))
+	if label.script_changed.is_connected(_on_label_script_changed.bind(label)):
+		label.script_changed.disconnect(_on_label_script_changed.bind(label))
 
 
 ## Gets called whenever any label on the scene gets a (new) script or is removed from it.
@@ -54,7 +61,8 @@ func _set_root(root: Node) -> void:
 	_current_root = root
 	if _current_root == null:
 		await _check_root()
-	_current_root.child_entered_tree.connect(_on_child_entered_tree.bind())
+	if !_current_root.child_entered_tree.is_connected(_on_child_entered_tree.bind()):
+			_current_root.child_entered_tree.connect(_on_child_entered_tree.bind())
 
 
 ## If no root is found (all scenes are closed) it keeps checking until one is opened.
@@ -71,10 +79,10 @@ func _check_nodes_in_scene() -> void:
 	nodes_to_check.append(_current_root)
 	while nodes_to_check.size() > 0:
 		var current_node: Node = nodes_to_check.pop_front()
-		if current_node == _current_root: ## It's either this or check if the signals are connected every single time.
-			continue
-		current_node.child_entered_tree.connect(_on_child_entered_tree.bind())
-		current_node.child_exiting_tree.connect(_on_child_exiting_tree.bind())
+		if !current_node.child_entered_tree.is_connected(_on_child_entered_tree.bind()):
+			current_node.child_entered_tree.connect(_on_child_entered_tree.bind())
+		if !current_node.child_exiting_tree.is_connected(_on_child_exiting_tree.bind()):
+			current_node.child_exiting_tree.connect(_on_child_exiting_tree.bind())
 		_check_label(current_node)
 		if current_node.get_child_count() > 0:
 			for child_node in current_node.get_children():
@@ -85,7 +93,9 @@ func _check_nodes_in_scene() -> void:
 ## If the node has the script already attached, it sets the defaults directly.
 func _check_label(node: Node) -> void:
 	if node is Label or node is RichTextLabel:
-		node.tree_exiting.connect(_on_label_exiting_tree.bind(node))
-		node.script_changed.connect(_on_label_script_changed.bind(node))
+		if !node.tree_exiting.is_connected(_on_label_exiting_tree.bind(node)):
+			node.tree_exiting.connect(_on_label_exiting_tree.bind(node))
+		if !node.script_changed.is_connected(_on_label_script_changed.bind(node)):
+			node.script_changed.connect(_on_label_script_changed.bind(node))
 		if node is LabelAutoSizer or node is RichLabelAutoSizer:
 			node.set_editor_defaults()
